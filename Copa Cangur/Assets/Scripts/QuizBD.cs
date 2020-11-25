b57db28ad93c020fb2486c306831a2568a298770
jfeliu16 <74668083+jfeliu16@users.noreply.github.com>
2020-11-25 17:20:14 +0100

    V 0.001
    Copa_Cangur

--- Copa Cangur/Assets/Scripts/QuizBD.cs
+++ Copa Cangur/Assets/Scripts/QuizBD.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class QuizBD : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private List<Question> m_questionList = null;
 }
